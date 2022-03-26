c6400c95084d5160810ab4e8f4d4b8bfcfe1d5b3
Sorin Catana <16884024+scatana@users.noreply.github.com>
2022-03-26 15:27:21 -0400

    added unit tests

--- src/app/navbar/navbar.component.spec.ts
+++ src/app/navbar/navbar.component.spec.ts
@@ -2,6 +2,9 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NavbarComponent } from './navbar.component';
 
+const NAVBAR_DISPLAY_THRESHOLD = 112;
+const WINDOW_SCROLL_Y_POSITION = 10;
+
 describe('NavbarComponent', () => {
   let component: NavbarComponent;
   let fixture: ComponentFixture<NavbarComponent>;
@@ -26,6 +29,13 @@ describe('NavbarComponent', () => {
     expect(component.isMenuClosed).toBe(true);
   });
 
+  it('should try to toggle the navbar when the window is scrolled', () => {
+    const spyToggleNavbar = spyOn(component, 'toggleNavbar');
+
+    window.dispatchEvent(new Event('scroll'));
+    expect(spyToggleNavbar).toHaveBeenCalled();
+  });
+
   it('should close the menu when the window is resized', () => {
     const spyCloseMenu = spyOn(component, 'closeMenu').and.callThrough();
 
@@ -35,6 +45,22 @@ describe('NavbarComponent', () => {
     window.dispatchEvent(new Event('resize'));
     expect(spyCloseMenu).toHaveBeenCalled();
     expect(component.isMenuClosed).toBe(true);
+  });
+
+  describe('toggleNavbar()', () => {
+    it('should toggle the navbar based on the scrolling offset', () => {
+      window.scrollY = WINDOW_SCROLL_Y_POSITION;
+      component.toggleNavbar();
+      expect(component.isNavbarHidden).toBe(true);
+
+      window.scrollY = NAVBAR_DISPLAY_THRESHOLD;
+      component.toggleNavbar();
+      expect(component.isNavbarHidden).toBe(true);
+
+      window.scrollY = NAVBAR_DISPLAY_THRESHOLD + 1;
+      component.toggleNavbar();
+      expect(component.isNavbarHidden).toBe(false);
+    });
   });
 
   describe('toggleMenu()', () => {
